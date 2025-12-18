1f9ac1507d27a31e90df69af6304622230be1356
bala-amypo <bala@amypo.in>
2025-12-18 09:41:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/PersonProfileController.java
+++ src/main/java/com/example/demo/controller/PersonProfileController.java
@@ -3,8 +3,17 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.example.demo.model.PersonProfile;
+import com.example.demo.service.PersonProfileService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RestController
+
 public class PersonProfileController{
-    @P
+     @Autowired
+    PersonProfileService ser;
+   @PostMapping("/api/persons")
+   public Sentity createData(@RequestBody Sentity ss)
+   {
+        return ser.createData(ss);
+   }
 }
