b3260d802e5ec5fcd2a40aeaa5fc6561314622c3
bala-amypo <bala@amypo.in>
2025-12-18 08:38:54 +0000

    User commit

--- src/main/java/com/example/demo/model/PersonProfile.java
+++ src/main/java/com/example/demo/model/PersonProfile.java
@@ -4,8 +4,8 @@ import java.time.LocalDateTime;
 import jakarta.persistence.*;
 @Entity
 public class PersonProfile{
-    @Id;
-    @GenertedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
     private String personType;
     @Column(unique=true)
@@ -16,4 +16,65 @@ public class PersonProfile{
     private String department;
     private Boolean relationshipDeclared=false;
     private  LocalDateTime createdAt;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getPersonType() {
+        return personType;
+    }
+    public void setPersonType(String personType) {
+        this.personType = personType;
+    }
+    public String getReferenceId() {
+        return referenceId;
+    }
+    public void setReferenceId(String referenceId) {
+        this.referenceId = referenceId;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public Boolean getRelationshipDeclared() {
+        return relationshipDeclared;
+    }
+    public void setRelationshipDeclared(Boolean relationshipDeclared) {
+        this.relationshipDeclared = relationshipDeclared;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public PersonProfile(long id, String personType, String referenceId, String fullName, String email, String department,
+            Boolean relationshipDeclared, LocalDateTime createdAt) {
+        this.id = id;
+        this.personType = personType;
+        this.referenceId = referenceId;
+        this.fullName = fullName;
+        this.email = email;
+        this.department = department;
+        this.relationshipDeclared = relationshipDeclared;
+        this.createdAt = createdAt;
+    }
+    public PersonProfile() {
+    }   
 }
