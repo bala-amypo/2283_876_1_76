cf512ecfe4505ea3a80b335f9737df9f6ebd3dfc
bala-amypo <bala@amypo.in>
2025-12-18 08:06:18 +0000

    User commit

--- src/main/java/com/example/demo/model/PersonProfile.java
+++ src/main/java/com/example/demo/model/PersonProfile.java
@@ -1,9 +1,11 @@
 package com.example.demo.model
 public class PersonProfile{
     @Id;
-    @Gerenera
     private long id;
     private String personType;
     @Column(unique="referenceId")
-
+    private String referenceId;
+    private String fullName;
+    @Column(unique="referenceId")
+    private String ;
 }
