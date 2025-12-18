56c08cfd25fc418c18f1ebacf25ad588c6b15afa
bala-amypo <bala@amypo.in>
2025-12-18 09:36:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/PersonProfileController.java
+++ src/main/java/com/example/demo/controller/PersonProfileController.java
@@ -1,4 +1,9 @@
 package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class PersonProfileController{
     @P
