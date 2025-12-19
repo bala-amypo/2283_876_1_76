a88bed1627d666d3abe784f9025735d619c8b6d6
bala-amypo <bala@amypo.in>
2025-12-19 08:22:57 +0000

    User commit

--- src/main/java/com/example/demo/service/Imp/PersonProfileServiceimp.java
+++ src/main/java/com/example/demo/service/Imp/PersonProfileServiceimp.java
@@ -31,7 +31,7 @@ public class PersonProfileServiceimp implements PersonProfileService{
     @Override
     public PersonProfile updateRelationshipDeclared(Long id,Boolean declared)
     {
-        PersonProfile ss=getPersonById(id);
+       Optional<PersonProfile> ss=getPersonById(id);
         ss.setRelationshipDeclared(declared);
         return rep.save(ss); 
     }
