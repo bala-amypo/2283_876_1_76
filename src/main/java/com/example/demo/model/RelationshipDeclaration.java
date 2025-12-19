de1db398c1e7a10652fd9eb841617838cbe560c8
bala-amypo <bala@amypo.in>
2025-12-19 10:04:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/RelationshipDeclaration.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+@Entity
+public class RelationshipDeclaration{
+    @Id
+    private Long id;
+    private Long id;
+
+}
