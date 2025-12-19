96f3d05df91afd55c8a4f8ee5cc181dad198f74a
bala-amypo <bala@amypo.in>
2025-12-19 17:10:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/RelationshipDeclarationRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import java.util.*;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.RelationshipDeclaration;
+@Repository
+public interface RelationshipDeclaration extends JpaRepository <PersonProfile,Long>{
+    Optional<PersonProfile>findByReferenceId(String referenceId);
+}
--- src/main/java/com/example/demo/service/Imp/RelationshipDeclarationServiceimp.java
+++ src/main/java/com/example/demo/service/Imp/RelationshipDeclarationServiceimp.java
@@ -7,4 +7,16 @@ import com.example.demo.model.RelationshipDeclaration;
 import com.example.demo.service.RelationshipDeclarationService;
 
 import com.example.demo.repository.PersonProfileRepository;
-@
+@Service
+
+
+public class PersonProfileServiceimp implements PersonProfileService{
+    @Autowired
+     PersonProfileRepository rep;
+
+    @Override
+    public PersonProfile createPerson(PersonProfile ss)
+    {
+        return rep.save(ss);
+    }
+}
