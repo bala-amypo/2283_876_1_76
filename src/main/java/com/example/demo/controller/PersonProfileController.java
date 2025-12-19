e344fce56e2564c59d678dfbbad635f268ce2db2
bala-amypo <bala@amypo.in>
2025-12-19 05:41:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/PersonProfileController.java
+++ src/main/java/com/example/demo/controller/PersonProfileController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.PersonProfile;
@@ -28,6 +29,11 @@ public class PersonProfileController{
      return ser.getPersonById(id);
    }
    @GetMapping("/")
+   public List<PersonProfile>getAllPersons()
+   {
+     return ser.getAllPersons();
+   }
+   @PutMapping("/{id}")
    public List<PersonProfile>getAllPersons()
    {
      return ser.getAllPersons();
