19db32eb6fdc8a8d5d23444fe487f27a2c4a3d90
bala-amypo <bala@amypo.in>
2025-12-20 13:47:55 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,9 +1,14 @@
 package com.example.demo.security;
 
-import com.example.demo.security
+import com.example.demo.security.UserPrincipal;
+
 public class JwtTokenProvider
 {
-    public void generateToken(Userprincipal up)
+    public void generateToken(UserPrincipal up)
+    {
+
+    }
+    public void validateToken(String token)
     {
 
     }
