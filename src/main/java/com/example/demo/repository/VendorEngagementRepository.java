7fb43a4a380a84ecadd26ce341d5a8c5fbf80a80
bala-amypo <bala@amypo.in>
2025-12-20 06:23:38 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorEngagementRepository.java
+++ src/main/java/com/example/demo/repository/VendorEngagementRepository.java
@@ -8,4 +8,5 @@ import com.example.demo.model.VendorEngagementRecord;
 @Repository
 public interface VendorEngagementRepository extends JpaRepository <VendorEngagementRecord,Long>{
    List<VendorEngagementRecord>findByEmployeeId(Long employeeId);
+   List<VendorEngagementRecord>findByVendorId(Long vendorId);
 }
