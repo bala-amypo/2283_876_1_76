03a5d90a980f8b17345b87af9dd82b5ff90f6bd1
bala-amypo <bala@amypo.in>
2025-12-20 15:29:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/PersonProfileController.java
+++ src/main/java/com/example/demo/controller/PersonProfileController.java
@@ -18,42 +18,42 @@ import com.example.demo.service.PersonProfileService;
 @RestController
 @RequestMapping("/api/persons")
 public class PersonProfileController{
-    private final PersonProfileService ser;
+	private final PersonProfileService ser;
 
-    public PersonProfileController(PersonProfileService ser) {
-        this.ser = ser;
-    }
+	public PersonProfileController(PersonProfileService ser) {
+		this.ser = ser;
+	}
 
 
-   @PostMapping
-   public PersonProfile createPerson(@RequestBody PersonProfile ss)
-   {
-        return ser.createPerson(ss);
-   }
-   @GetMapping("/{id}")
-   public Optional<PersonProfile> getPersonById(@PathVariable Long id)
-   {
-     return ser.getPersonById(id);
-   }
-   @GetMapping
-   public List<PersonProfile>getAllPersons()
-   {
-     return ser.getAllPersons();
-   }
-   @PutMapping("/{id}/relationship-declared")
-   public PersonProfile updateRelationshipDeclared(@PathVariable Long id,@RequestParam Boolean declared)
-   {
-    return ser.updateRelationshipDeclared(id,declared);
-   }
-   @GetMapping("/lookup/{referenceId}")
-   public PersonProfile findByReferenceId(@PathVariable String referenceId)
-   {
-    return ser.findByReferenceId(referenceId);
-   }
-   @GetMapping("/lookup/{refId}")
-    public PersonProfile lookup(@PathVariable String refId) {
-        return ser.findByReferenceId(refId);
-    }
+	@PostMapping
+	public PersonProfile createPerson(@RequestBody PersonProfile ss)
+	{
+		return ser.createPerson(ss);
+	}
+	@GetMapping("/{id}")
+	public Optional<PersonProfile> getPersonById(@PathVariable Long id)
+	{
+		return ser.getPersonById(id);
+	}
+	@GetMapping
+	public List<PersonProfile>getAllPersons()
+	{
+		return ser.getAllPersons();
+	}
+	@PutMapping("/{id}/relationship-declared")
+	public PersonProfile updateRelationshipDeclared(@PathVariable Long id,@RequestParam Boolean declared)
+	{
+		return ser.updateRelationshipDeclared(id,declared);
+	}
+	@GetMapping("/lookup/{referenceId}")
+	public PersonProfile findByReferenceId(@PathVariable String referenceId)
+	{
+	return ser.findByReferenceId(referenceId);
+	}
+	@GetMapping("/lookup/{refId}")
+	public PersonProfile lookup(@PathVariable String refId) {
+	return ser.findByReferenceId(refId);
+	}
 
 
 }
