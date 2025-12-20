bc678dc71a2995f6f6cc4c8b3661a54c97fd5731
bala-amypo <bala@amypo.in>
2025-12-20 04:50:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Imp/VendorEngagementServiceImp.java
@@ -0,0 +1,7 @@
+package com.example.demo.service.Imp;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.RelationshipDeclaration;
+import com.example.demo.service.RelationshipDeclarationService;
