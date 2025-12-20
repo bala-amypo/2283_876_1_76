950f7320faf5eb476cbe92409c67de10be79b4e2
bala-amypo <bala@amypo.in>
2025-12-20 06:38:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/ConflictCase.java
@@ -0,0 +1,80 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+
+public class Sentity 
+{
+    @Id
+   private Long id;
+   private Long primaryPersonId;
+   private Long secondaryPersonId;
+   private String triggerSource;
+   private String riskLevel;
+   private String details;
+   private String status="OPEN";
+   private LocalDateTime detectedAt;
+   public Long getId() {
+    return id;
+   }
+   public void setId(Long id) {
+    this.id = id;
+   }
+   public Long getPrimaryPersonId() {
+    return primaryPersonId;
+   }
+   public void setPrimaryPersonId(Long primaryPersonId) {
+    this.primaryPersonId = primaryPersonId;
+   }
+   public Long getSecondaryPersonId() {
+    return secondaryPersonId;
+   }
+   public void setSecondaryPersonId(Long secondaryPersonId) {
+    this.secondaryPersonId = secondaryPersonId;
+   }
+   public String getTriggerSource() {
+    return triggerSource;
+   }
+   public void setTriggerSource(String triggerSource) {
+    this.triggerSource = triggerSource;
+   }
+   public String getRiskLevel() {
+    return riskLevel;
+   }
+   public void setRiskLevel(String riskLevel) {
+    this.riskLevel = riskLevel;
+   }
+   public String getDetails() {
+    return details;
+   }
+   public void setDetails(String details) {
+    this.details = details;
+   }
+   public String getStatus() {
+    return status;
+   }
+   public void setStatus(String status) {
+    this.status = status;
+   }
+   public LocalDateTime getDetectedAt() {
+    return detectedAt;
+   }
+   public void setDetectedAt(LocalDateTime detectedAt) {
+    this.detectedAt = detectedAt;
+   }
+   public Sentity(Long id, Long primaryPersonId, Long secondaryPersonId, String triggerSource, String riskLevel,
+        String details, String status, LocalDateTime detectedAt) {
+    this.id = id;
+    this.primaryPersonId = primaryPersonId;
+    this.secondaryPersonId = secondaryPersonId;
+    this.triggerSource = triggerSource;
+    this.riskLevel = riskLevel;
+    this.details = details;
+    this.status = status;
+    this.detectedAt = detectedAt;
+   }
+   public Sentity() {
+   }
+   
+}
