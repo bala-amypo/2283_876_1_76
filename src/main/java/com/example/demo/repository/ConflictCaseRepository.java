c8ead0a3abe37ae01b58470eca6693724002bc4b
bala-amypo <bala@amypo.in>
2025-12-20 10:16:03 +0000

    User commit

--- src/main/java/com/example/demo/repository/ConflictCaseRepository.java
+++ src/main/java/com/example/demo/repository/ConflictCaseRepository.java
@@ -1,4 +1,9 @@
-ConflictFlagRepositoryConflictFlagRepositoryConflictFlagRepositoryConflictFlagRepositoryConflictFlagRepositoryConflictFlagRepository
+package com.example.demo.repository;
+
+import java.util.*;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.model.ConflictCase;
 
 @Repository
