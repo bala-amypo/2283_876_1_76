67d6d3582e4d77fb0abd11d2ee0a1f4848f4581b
bala-amypo <bala@amypo.in>
2025-12-20 04:25:33 +0000

    User commit

--- src/main/java/com/example/demo/model/VendorEngagementRecord.java
+++ src/main/java/com/example/demo/model/VendorEngagementRecord.java
@@ -1,2 +1,11 @@
 package com.example.demo.model;
 
+public class VendorEngagementRecord{
+    @Id
+    private Long id;
+    private Long employeedId;
+    private Long vendorId;
+    private String engagementType;
+    private double ;
+
+}
