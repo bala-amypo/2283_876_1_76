a6b6970e94a0d9f95d8005cdbb52470de10184d6
bala-amypo <bala@amypo.in>
2025-12-20 04:38:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/VendorEngagementController.java
+++ src/main/java/com/example/demo/controller/VendorEngagementController.java
@@ -1,1 +1,14 @@
 package com.example.demo.controller;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.VendorEngagementRecord;
+import com.example.demo.service.VendorEngagementService;
+
+
+@RestController
+@RequestMapping("/api/engagements")
+
+public class VendorEngagementCon
