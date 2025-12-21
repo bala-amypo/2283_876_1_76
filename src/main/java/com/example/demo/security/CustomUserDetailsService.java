b1b8f3909e1fcca7016d1201568c216bee6aa35c
bala-amypo <bala@amypo.in>
2025-12-21 13:05:05 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -9,23 +9,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-
-    // @Override
-    // public UserDetails loadUserByUsername(String username)
-    //         throws UsernameNotFoundException {
-
-    //     // dummy in-memory user for tests
-    //     List<String> roles = List.of("ROLE_USER");
-
-    //     return new UserPrincipal(
-    //             username,
-    //             "password",
-    //             roles.stream()
-    //                     .map(SimpleGrantedAuthority::new)
-    //                     .collect(Collectors.toList())
-    //     );
-    // }
-
     // helper for tests
     public UserDetails loadUser(String username, List<String> roles) {
         return new UserPrincipal(
